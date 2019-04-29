5d15c3dc3f1b8a8d43a62ff94b4df1a9dc6cc36c
Paige Lieske <paigeproctor@comcast.net>
2019-04-29 17:38:55 -0400

    currency format correction

--- paige-a-zon.js
+++ paige-a-zon.js
@@ -16,16 +16,18 @@ connection.connect(function (err) {
     inventory();
 });
 
+// var total = new Number(parseFloat(res[0].price * qtyBuy)).toFixed(2)
+
 function inventory() {
     console.log("Welcome to Paige-a-Zon");
-    console.log("We have the following items: ");
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         var display = new table({
             head: ["Item ID", "Product Name", "Department", "Price", "In Stock"],
-            colWidths: [10, 30, 30, 10, 10]
+            colWidths: [10, 30, 30, 15, 10]
         });
         for (var i = 0; i < res.length; i++) {
+            res[i].price = new Number(parseFloat(res[i].price)).toFixed(2);
             display.push([res[i].item_id, res[i].product_name, res[i].department_name, "$" + res[i].price, res[i].stock_quantity]);
         }
         console.log(display.toString());
@@ -41,15 +43,10 @@ function purchase() {
                 type: "input",
                 message: "What is the ID of the item you would like to purchase? [Quit with Q]"
             }
-            // {
-            //     name: "quantity",
-            //     type: "input",
-            //     message: "How many would you like"
-            // }
         ])
         .then(function (answer) {
             var productToBuy = answer.purchaseID;
-            if (productToBuy === "Q") {
+            if (productToBuy == "Q" || productToBuy == "q") {
                 console.log("Come back soon!");
                 connection.end();
             }
@@ -61,15 +58,6 @@ function purchase() {
                 })
                     .then(function (number) {
                         var qtyBuy = number.quantity;
-                        // if (qtyBuy > 0 && qtyBuy < 1) {
-                        //     console.log("You have purchased " + qtyBuy + " " + productToBuy);
-                        // }
-                        // else if (qtyBuy > 1) {
-                        //     console.log("You have purchased " + qtyBuy + " " + productToBuy + "'s");
-                        // }
-                        // else {
-                        //     console.log("I guess you changed your mind");
-                        // }
                         makeSale(productToBuy, qtyBuy);
                     })
             }
@@ -78,19 +66,21 @@ function purchase() {
 
 function makeSale(productToBuy, qtyBuy) {
     connection.query("SELECT * FROM products WHERE item_id = " + parseInt(productToBuy), function (err, res) {
-        console.log(res[0].product_name);
+        console.log("Checking stock for item " + res[0].product_name + ".......");
         if (err) throw (err);
-        if(qtyBuy <= res[0].stock_quantity) {
-            console.log(qtyBuy);
-            var total = res[0].price * qtyBuy;
-            total = parseFloat(total);
-            console.log("Your item is in stock.");
-            console.log("The total for item number " + res[0].item_id + ", product " + res[0].product_name + ", is " + total + ".");
-            // connection.query("UPDATE products SET stock_quantity = stock_quantity - ${qtyBuy} WHERE item_id = ${productToBuy}");
+        if (qtyBuy <= res[0].stock_quantity) {
+            // console.log(qtyBuy);
+            var total = new Number(parseFloat(res[0].price * qtyBuy)).toFixed(2);
+            console.log("Congratulations - your item is in stock.");
+            console.log("The total for item number " + res[0].item_id + ", product " + res[0].product_name + ", is $" + total + ".");
+            connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [res[0].stock_quantity - qtyBuy, productToBuy], function (err) {
+                if (err) throw (err);
+            });
         }
         else {
             console.log("I'm sorry, we don't have enough stock to complete your order.  Please try again.");
         }
-        connection.end();
+        inventory();
+        // connection.end();
     })
 };
--- paigeazonSchema.sql
+++ paigeazonSchema.sql
@@ -17,10 +17,10 @@ USE paigeazon_db;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Krispy Kreme Coffee", "Food & Non-Alcoholic Beverages", 12.05, 4);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Coke Zero", "Food & Non-Alcoholic Beverages", 1.55, 6);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Whispering Angel", "Wine", 19.85, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Whispering Angel", "Wine", 19.80, 8);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Nobilo", "Wine", 11.85, 1);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Tank Top", "Clothing", 6.15, 27);
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Maxi Dress", "Clothing", 31.75, 2);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Maxi Dress", "Clothing", 31.50, 2);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("The Brady Bunch Party Game", "Toys & Games", 13.95, 9);
 INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Crate Creatures", "Toys & Games", 6.55, 3);
 
